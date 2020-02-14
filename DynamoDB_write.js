0e06791884fb8414f4a18ded6ae9c9e723326b62
akramIOT <sheriff.akram@gmail.com>
2020-02-13 18:05:22 -0800

    Created  records are stored in DynamoDB
    One record example of  Created records and stored in DynamoDB

---
+++ DynamoDB_write.js
@@ -0,0 +1,38 @@
+console.log('Loading function');
+var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB.DocumentClient();
+var table = "iotCatalog";
+//var table2 = "Sensor_State";
+
+  
+exports.handler = function(event, context) {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+   var params = {
+    TableName:table,
+    Item:{
+        "serialNumber": event.serialNumber,
+        "clientId": event.clientId,
+        "Occupancy_state": event.Occupancy_state,
+        "location": event.location,
+        "timestamp": event.timestamp,
+        "device": event.device,
+        "endpoint": event.endpoint,
+        "type": event.type,
+        "certificateId": event.certificateId,
+        "activationCode": event.activationCode,
+        "activated": event.activated,
+        "email": event.email
+        }
+    };
+
+    console.log("Adding a new Parking IoT Sensor into DynamoDB...");
+    dynamo.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add parking Sensor. Error JSON:", JSON.stringify(err, null, 2));
+            context.fail();
+        } else {
+            console.log("Added device:", JSON.stringify(data, null, 2));
+            context.succeed();
+        }
+    });
+}
